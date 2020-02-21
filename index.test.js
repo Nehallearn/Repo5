9a3f71bff8e8a7b47eadb29ec7fdab1f4b45f07a
Nehallearn <learn5151@gmail.com>
2020-02-21 22:56:15 +0000

    second commit

--- index.test.js
+++ index.test.js
@@ -1,10 +1,18 @@
 log = console.log
+assert = require('chai').assert;
 expect = require('chai').expect
 should = require('chai').should()
 _ = require('lodash')
 log("Yusra go to sleep")
 describe('First test', ()=>
 {
+    let num = 5;
+    let str = 'hello';
+
+    it('Check something', ()=>
+    {
+       assert.isNumber(str);
+    })    
 
 it('This is first test', ()=>
 {
