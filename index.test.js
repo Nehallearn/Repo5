fecf605f8511bf62ae6c62b3255c02bbbec4cee7
Nehallearn <learn5151@gmail.com>
2020-02-21 22:05:33 +0000

    First commit

---
+++ index.test.js
@@ -0,0 +1,21 @@
+log = console.log
+expect = require('chai').expect
+should = require('chai').should()
+_ = require('lodash')
+log("Yusra go to sleep")
+describe('First test', ()=>
+{
+
+it('This is first test', ()=>
+{
+   true.should.be.true;
+})
+it('Expect this to be true', ()=>
+
+{
+
+    expect(true).to.be.true;
+})
+
+}
+)
